1a77e0b47efa2b69eb0a2d35f6b800541dd2f386
rodridega <56453875+rodridega@users.noreply.github.com>
2022-10-19 23:51:38 -0300

    ready for render

--- funnel/views.py
+++ funnel/views.py
@@ -1,14 +1,43 @@
 from django.http import HttpResponse
 import datetime
 
+from django.template import Context, Template
+
+
+class Persona(object):
+
+    def __init__(self, nombre, apellido):
+
+        self.nombre = nombre
+        self.apellido = apellido
+
+
 def getAll(request):
-    return HttpResponse("Hola mundo")
+
+    p1 = Persona("Rodrigo", "Deganutti")
+
+    nombre = "Rodrigo"
+
+    doc_externo = open(
+        "C:/Users/Rodrigo/Desktop/Proyectos/test-django/funnel/plantillas/template.html")
+
+    plt = Template(doc_externo.read())
+
+    doc_externo.close()
+
+    contexto = Context({
+        "nombre": p1.nombre, "apellido": p1.apellido
+    })
+
+    documento = plt.render(contexto)
+    return HttpResponse(documento)
 
 
 def fecha(request):
     fechaActual = datetime.datetime.now()
 
     return HttpResponse(fechaActual)
+
 
 def edad(request, ano):
 
